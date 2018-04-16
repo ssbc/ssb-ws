8014b9556e09a83d8c118478f5659f384bc70c12
Dominic Tarr <dominic.tarr@gmail.com>
2018-04-15 22:50:28 -0700

    rewrite in terms of secret-stack transport

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-var MultiServer = require('multiserver')
 var WS = require('multiserver/plugins/ws')
-var SHS = require('multiserver/plugins/shs')
 var http = require('http')
-var muxrpc = require('muxrpc')
 var pull = require('pull-stream')
 var JSONApi = require('./json-api')
-
-var cap =
-  new Buffer('1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s=', 'base64')
-
-function toSodiumKeys(keys) {
-  return {
-    publicKey:
-      new Buffer(keys.public.replace('.ed25519',''), 'base64'),
-    secretKey:
-      new Buffer(keys.private.replace('.ed25519',''), 'base64'),
-  }
-}
 
 var READ_AND_ADD = [ //except for add, of course
   'get',
@@ -45,18 +30,9 @@ var READ_AND_ADD = [ //except for add, of course
 
 exports.name = 'ws'
 exports.version = require('./package.json').version
-exports.manifest = {
-  getAddress: 'sync'
-}
-
-function toId(id) {
-  if (typeof id !== 'string') {
-    return '@' + id.toString('base64') + '.ed25519' // isn't this available somewhere else?
-  } else throw new Error('toId() called on string. todo: clean this your mess.')
-}
+exports.manifest = {}
 
 exports.init = function (sbot, config) {
-
   var port
   if(config.ws)
     port = config.ws.port
@@ -64,54 +40,42 @@ exports.init = function (sbot, config) {
     port = 1024+(~~(Math.random()*(65536-1024)))
 
   var layers = []
-  var server = http.createServer(JSONApi(sbot, layers)).listen(port)
+  var server, ws_server
 
-  function _auth (id, cb) {
-    sbot.friends.get({source: sbot.id, dest: toId(id)}, function (err, follows) {
-      if(err) return cb(err)
-      else if(follows) cb(null, {allow: READ_AND_ADD, deny: null})
-      else cb()
+  function createServer (instance) {
+    if(server) return server
+    server = http.createServer(JSONApi(sbot, layers)).listen(port+instance)
+    ws_server = WS({
+      server: server, port: port, host: config.host || 'localhost'
     })
+    return server
   }
 
-  var ms = MultiServer([
-    [
-      WS({server: server, port: port, host: config.host || 'localhost'}),
-      SHS({
-        keys: toSodiumKeys(config.keys),
-        appKey: (config.caps && new Buffer(config.caps.shs, "base64")) || cap,
-        auth: function (id, cb) {
-          sbot.auth(toId(id), function (err, allowed) {
-            if(err || allowed) cb(err, allowed)
-            else _auth(id, cb)
-          })
-        },
-        timeout: config.timeout
-      })
-    ]
-  ])
-
-  var close = ms.server(function (stream) {
-    var manifest = sbot.getManifest()
-    var rpc = muxrpc({}, manifest)(sbot, stream.auth)
-    rpc.id = toId(stream.remote)
-    pull(stream, rpc.createStream(), stream)
+  sbot.auth.hook(function (fn, args) {
+    var id = args[0]
+    var cb = args[1]
+    fn(id, function (err, value) {
+      if(value === true)
+        sbot.friends.get({source: sbot.id, dest: toId(id)}, function (err, follows) {
+          if(err) return cb(err)
+          else if(follows) cb(null, {allow: READ_AND_ADD, deny: null})
+          else cb(null, true)
+        })
+      else
+        cb(err, value)
+    })
   })
 
-  //close when the server closes.
-  sbot.close.hook(function (fn, args) {
-    close()
-    fn.apply(this, args)
+  sbot.multiserver.transport(function (instance) {
+    console.log('create ws server'.toUpperCase())
+    createServer(instance)
+    return ws_server
   })
 
   return {
-    getAddress: function () {
-      return ms.stringify()
-    },
     use: function (handler) {
       layers.push(handler)
     }
-
   }
 }
 
