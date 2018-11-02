0e56f92982f73eaa07ab4f2fd96002f10280a585
Dominic Tarr <dominic.tarr@gmail.com>
2018-11-02 20:45:14 +0530

    support multiple web servers

--- index.js
+++ index.js
@@ -42,26 +42,8 @@ exports.init = function (sbot, config) {
     port = 1024+(~~(Math.random()*(65536-1024)))
 
   var layers = []
-  var ws_servers = []
 
-  function createServer (config, cb) {
-    var _host = config.host || config.scope && scopes.host(config.scope) || host || 'localhost'
-    var _port = config.port || port
-    var ws_server
-    var key = _host + ':' + _port
-    if(ws_server = ws_servers[key]) return ws_server
-    //debug('listening on host=%s port=%d', _host, _port)
-    var server = http.createServer(JSONApi(sbot, layers)).listen(_port, _host, function(err) {
-      if (err) console.error('ssb-ws failed to listen on ' + _host + ':' + _port, err)
-      else console.log('Listening on ' + _host + ':' + _port, '(ssb-ws)')
-      if (cb) return cb(err)
-    })
-    ws_server = WS(Object.assign({
-      server: server, port: _port, host: _host
-    }, config))
-    ws_servers[key] = ws_server
-    return ws_server
-  }
+  var handlers = JSONApi(sbot, layers)
 
   sbot.auth.hook(function (fn, args) {
     var id = args[0]
@@ -77,11 +59,21 @@ exports.init = function (sbot, config) {
         cb(err, value)
     })
   })
-
+  var c = 0
   sbot.multiserver.transport({
     name: 'ws',
     create: function (config) {
-      return createServer(config)
+      if(c++) throw new Error('more than one server, same port:'+JSON.stringify(config))
+      var _host = config.host || 'localhost'
+      var _port = config.port || port
+      //debug('listening on host=%s port=%d', _host, _port)
+      var server = http.createServer(handlers).listen(_port, _host, function(err) {
+        if (err) console.error('ssb-ws failed to listen on ' + _host + ':' + _port, err)
+        else console.log('Listening on ' + _host + ':' + _port, '(ssb-ws)')
+      })
+      return WS(Object.assign({
+        server: server, port: _port, host: _host
+      }, config))
     }
   })
 
