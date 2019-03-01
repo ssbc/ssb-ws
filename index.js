9adb4f9001854509eb0cbcd881cb70d0a0af06ab
Christian Bundy <christianbundy@fraction.io>
2019-03-02 12:46:28 +1300

    Remove special permissions from ssb-ws
    It's unclear to me why these were added to the ssb-ws interface, but
    they seem that they may allow local peers to query all messages
    (including decrypted private messages) and should probably be
    implemented somewhere else if necessary. This commit also removes unused
    variables, which makes it less difficult to grok the code.

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
 var WS = require('multiserver/plugins/ws')
-var http = require('http')
-var pull = require('pull-stream')
 var JSONApi = require('./json-api')
-var scopes = require('multiserver-scopes')
-
-var READ_AND_ADD = [ //except for add, of course
-  'get',
-  'getLatest',
-  'createLogStream',
-  'createUserStream',
-
-  'createHistoryStream',
-  'getAddress',
-
-  'links',
-
-  'blobs.add',
-  'blobs.size',
-  'blobs.has',
-  'blobs.get',
-  'blobs.changes',
-  'blobs.createWants',
-
-  'add',
-
-  'query.read',
-  'links2.read'
-]
 
 exports.name = 'ws'
 exports.version = require('./package.json').version
 exports.manifest = {}
 
 exports.init = function (sbot, config) {
-  var port, host
+  var port
   if(config.ws) {
     port = config.ws.port
-    host = config.ws.host || config.host
   }
   if(!port)
     port = 1024+(~~(Math.random()*(65536-1024)))
@@ -51,23 +23,14 @@ exports.init = function (sbot, config) {
     var id = args[0]
     var cb = args[1]
     fn(id, function (err, value) {
-      if(value === true)
-        sbot.friends.get({source: sbot.id, dest: toId(id)}, function (err, follows) {
-          if(err) return cb(err)
-          else if(follows) cb(null, {allow: READ_AND_ADD, deny: null})
-          else cb(null, true)
-        })
-      else
-        cb(err, value)
+      cb(err, value)
     })
   })
-  var c = 0
   sbot.multiserver.transport({
     name: 'ws',
     create: function (config) {
       var _host = config.host || 'localhost'
       var _port = config.port || port
-      //debug('listening on host=%s port=%d', _host, _port)
       return WS(Object.assign({
         port: _port, host: _host,
         handler: config.http !== false ? handlers : no_handler
@@ -81,5 +44,3 @@ exports.init = function (sbot, config) {
     }
   }
 }
-
-
